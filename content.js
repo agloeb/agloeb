2eba1da1f9c73ac8ce053eb19f91166c23877c02
agloeb <147872579+agloeb@users.noreply.github.com>
2026-01-23 15:06:50 -0700

    big turkey

---
+++ content.js
@@ -0,0 +1,18 @@
+const entries = [
+  {
+    title: "eliott's pheasant",
+    text: `    I once hit a pheasant and did some damage. Before that was another with buckshot stuck in its breast, still hard after the oven. But latley it’s been the pheasant above me wearing red feathers and ring to bed. Our turkey and prairie chicks wear their suits and jewels too but dress down for the night in woodland patterend pajamas. My pheasant walks on a razor and puts my brother to sleep in his bed touching the closet door and pocket door. It all helps hide the red LEDs and unmatched underwear. I’m in clover that you lay down the cakes in the early morning before everyone takes off their pajamas and puts their tiny faces on for valentines.`
+  },
+  {
+    title: "my acres are cooling",
+    text: `Tammy is suddenly riding a bike. She looks strange.`
+  },
+  {
+    title: "permabog",
+    text: `Stopped by a semi then a house after taking advice from the couple who made us hotdogs in the Walmart parking lot. Then we’re ninety-two in the summer getting killed by kidneys, getting letters from everyone so sorry for us. The biggest feeling I have she has thought about with more math and bees in the chimney, dealing with that all alone, and with more dust in the dining room than in the vacuum tubes behind the walls. You were such a good piece of pork now you’re all shrunk from the heat. `
+  },
+  {
+    title: "dancing (latino)",
+    text: 'Always three men in the back of my car snapping a doo wop song. Took one year for their thoughts on voles to come true in my front yard. Last week I saw Chris the tall one at the store. My tooth fell out here yesterday, he said. He animal walked through the aisles until he found it under the pretzels. There are no S’s when singing doo wah’s so his singing is unaffected. Bogdan the middle one is a biological telephone. He doesn’t talk but the master of a spiritual community in Ouray speaks through him. Bogdan and Chris will live very long lives is the report from Ouray. The third doo wopper’s life will be cut respectably short by a stomach bug. The most difficult thing about having backup singers and snappers in the back of your car is that you can’t offer to haul or carpool. Not even for the fact that your back bench seat is full but their tune wears on your relationships.'
+  }
+];
